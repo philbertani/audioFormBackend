9de1eb03f314efa7d4da0df9508505ef5a2bd682
Philip Bertani <philip.bertani@gmail.com>
2023-01-27 14:35:26 -0500

    added live fudge factor

--- client/index.js
+++ client/index.js
@@ -3,20 +3,70 @@ import axios from "axios"
 //browser always complains unless we first create the AC after a button click 
 //webkit is the mac/safari/ios version of this stuff
 const AC = new (window.AudioContext || window.webkitAudioContext)();
+let fudge = 0  //in seconds
+let timeOffset = .05
+let currentPlayPosition = 0
 
-function playSound(source, time) {
+function playSound(source, time, fudge) {
   //connect to gainNodes to control relative volume
   source.connect(AC.destination);
-  //source[source.start ? "start" : "noteOn"](time);
-  source.start(time)
+  source.start(time, Math.max( currentPlayPosition + fudge, 0) )
+}
+
+function stopSound(source,time) {
+  source.stop(time)
+  source.disconnect()
 }
 
 const play2filesElem = document.getElementById('play2files')
 play2filesElem.addEventListener('click',function() {play2() })
+const increaseFudge = document.getElementById('nudgeForwards')
+const decreaseFudge = document.getElementById('nudgeBackwards')
+const fudgeOut = document.getElementById('fudge')
+
+//increaseFudge.addEventListener('click',function(){ handleFudge(1) })
+//decreaseFudge.addEventListener('click',function(){ handleFudge(-1)})
+fudgeOut.addEventListener('change',function(){restart()})
+
+function restart() {
+  if ( !started ) return
+
+  started = false
+  play = 0
+
+  const currentTime = AC.currentTime
+  const src1 = sources[0]
+  const src2 = sources[1]
+
+  stopSound(src1,currentTime)
+  stopSound(src2,currentTime)
+
+  play2(0)
+}
+
+function handleFudge(direction) {
+
+  fudge += direction * timeOffset
+  fudgeOut.value = Math.round(fudge*100)/100
+
+  if ( !started ) return
+
+  started = false
+  play = 0
+
+  const currentTime = AC.currentTime
+  const src1 = sources[0]
+  const src2 = sources[1]
+
+  stopSound(src1,currentTime)
+  stopSound(src2,currentTime)
+  //play2(currentTime)
+}
+
 let play = 0
 let started = false
 
-async function play2() {
+async function play2(restartTime) {
 
   if ( play === 1 ) {
     play = 0
@@ -34,22 +84,27 @@ async function play2() {
     const tempo = 80; // BPM (beats per minute)
     const eighthNoteTime = 60 / tempo / 2;
   
-    //this was the wrong direction, we needed the binary arraybuffer
-    //const src1 = AC.createMediaElementSource(audioElems[0])
-    //const src2 = AC.createMediaElementSource(audioElems[1])
-  
-    const src1 = sources[0]
-    const src2 = sources[1]
+    sources.length = 0  //reset the array
+    const src1 = AC.createBufferSource();
+    src1.buffer = audioBuffers[0];
+    const src2 = AC.createBufferSource();
+    src2.buffer = audioBuffers[1]
+    sources.push(src1,src2)
 
-    playSound(src1, startTime + 0.25);
-    playSound(src2, startTime);
+    fudge = fudgeOut.value
+    console.log("fudge is:" , fudge)
+    //all of the AudioBuffer timing is in seconds as opposed to the usual milliseconds
+    playSound(src1, startTime, 0);
+    playSound(src2, startTime, fudge);
+
     started = true
   }
-  
+
 };
 
 console.log('trying to get 2 audio files')
 const sources = []
+const audioBuffers = []   //we actually need the whole audioBuffers since we need to recreate sources on changes
 
 async function getFiles(filename, audioId) {
 
@@ -82,16 +137,10 @@ async function getFiles(filename, audioId) {
 
       //here is the AudioContext way of doing things with sound...
       const source = AC.createBufferSource();
-
       //wait for both with Promises.all at some point
       const xxx = await AC.decodeAudioData(ab);
-
-      source.buffer = xxx;
-      source.connect(AC.destination);
-
       //save them so we can use them later on button click
-      sources.push(source);
-      console.log(xxx);
+      audioBuffers.push(xxx)
 
     } catch (err) {}
 
